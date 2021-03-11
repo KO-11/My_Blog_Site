a9df35a8fee556ee94aa691b2fac4a4cf0dd7eef
Kevin Olson <kevin.m.olson11@gmail.com>
2021-03-11 10:24:54 -0800

    Added basic test file

---
+++ test/index.test.js
@@ -0,0 +1,17 @@
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+const server = require('../server');
+const should = chai.should();
+const BlueBird = require('bluebird');
+
+chai.use(chaiHttp);
+
+describe('blog server', () => {
+
+  it('should return index.html file', async () => {
+      const response = await chai.request('http://localhost:3000').get('/')
+      // response.should.have.status(200);
+      response.type.should.eql('text/html');
+  });
+
+});
